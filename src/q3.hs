6ebebde44413fb1afbae23830cb62fee1b0b0910
tmacksf <33499837+tmacksf@users.noreply.github.com>
2023-07-13 00:32:05 +0100

    Added another solution for q3

--- src/q3.hs
+++ src/q3.hs
@@ -2,5 +2,11 @@
 
 kth :: [a] -> Integer -> a
 kth [] y = error "out of bounds"
-kth (x:_) 0 = x
+kth (x:_) 1 = x
 kth (x:xs) y = kth xs (y-1)
+
+
+-- TODO: Figure out how this works
+kth' xs n 
+  | length xs < n = error "out of bounds"
+  | otherwise = last $ take n xs
