3d22296763c789b7ae93e13c5bbdc6d0d2afa084
tmacksf <33499837+tmacksf@users.noreply.github.com>
2023-07-12 21:50:59 +0100

    Q1 done!

--- src/q1.hs
+++ src/q1.hs
@@ -1,1 +1,8 @@
-doubleme x = x + x
+-- Find the last element of a list.
+-- not using the last function
+
+-- Done with recursion
+last' :: [a] -> a
+last' [] = error "no last in empty list"
+last' (x:[]) = x
+last' (x:xs) = last (xs) 
