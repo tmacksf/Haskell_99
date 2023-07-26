7ddf39118386c4c601a1139a31ef102889050c00
tmacksf <33499837+tmacksf@users.noreply.github.com>
2023-07-26 21:55:18 +0300

    Q9
    Added more solutions.

--- src/q9.hs
+++ src/q9.hs
@@ -12,3 +12,15 @@ bruh [x] y = if x == y then 1 else 0
 bruh (x:xs) y
   | x == y = 1 + bruh xs y
   | otherwise = 0 
+
+pack'' :: (Eq a) => [a] -> [[a]]
+pack'' [] = [] 
+pack'' [x] = [[x]]
+pack'' (x:xs) = [[x] ++ takeWhile (== x) xs] ++ pack'' (dropWhile (==x) xs)
+
+
+pack' :: (Eq a) => [a] -> [[a]]
+pack' = foldr fn []
+    where fn y [] = [[y]]
+          fn y (x:xs) = if y == head x then ((y:x):xs) else ([y]:x:xs) 
+
