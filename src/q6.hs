23cb0ccb1f20dcbc01e668c8ceee78c713377c27
tmacksf <33499837+tmacksf@users.noreply.github.com>
2023-07-26 15:39:16 +0300

    modified q6

--- src/q6.hs
+++ src/q6.hs
@@ -1,10 +1,15 @@
 -- Find out whether a list is a palindrome.
 
-isPalindrome :: [Int] -> Bool 
-isPalindrome [] = False 
+isPalindrome :: (Eq a) => [a] -> Bool 
+isPalindrome [] = True 
 isPalindrome [x] = True
 isPalindrome x 
   | length x `mod` 2 == 0 = 
-      (reverse $ take (length x`div` 2) x) == (drop (length x`div` 2) x)
+      (reverse $ take (length x `div` 2) x) == (drop (length x`div` 2) x)
   | length x `mod` 2 == 1 = 
       (reverse $ take (length x `div` 2) x) == (drop (length x `div` 2 + 1) x)
+
+isPalindrome' :: (Eq a) => [a] -> Bool
+isPalindrome' [] = True
+isPalindrome' [x] = True
+isPalindrome' x = x == reverse x
