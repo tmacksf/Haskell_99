e782abe1983da049a51e3505451a703fc688c075
tmacksf <33499837+tmacksf@users.noreply.github.com>
2023-07-27 13:41:53 +0300

    Q10

--- src/q10.hs
+++ src/q10.hs
@@ -1,8 +1,11 @@
 -- Run-length encoding of a list.
 -- (Huffman encoding)
+import Data.List
 
 encode :: (Eq a) => [a] -> [(Int, a)]
 encode [] = []
 encode [x] = [(1, x)]
 encode (x:xs) = [(1 + (length $ takeWhile (== x) xs), x)] ++ encode (dropWhile (== x) xs)
 
+-- done as a list comprehension
+encode' xs = [(length x, head x) | x <- group xs]
