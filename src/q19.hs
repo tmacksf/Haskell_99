131bd39f38ed6ccf676ad26911787a0c6e8ea611
tmacksf <33499837+tmacksf@users.noreply.github.com>
2023-08-02 14:00:39 +0100

    Q19
    Done.

---
+++ src/q19.hs
@@ -0,0 +1,7 @@
+-- Rotate a list N places to the left.
+
+rotate :: [a] -> Int -> [a] 
+rotate xs 0 = xs
+rotate xs n 
+    | n > 0 = drop n xs ++ take n xs
+    | n < 0 = drop (length xs + n) xs ++ take (length xs + n) xs 
